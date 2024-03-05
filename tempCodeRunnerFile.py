3ea8baf27ed86f7935b22a9c9ba38ba17f2df2c1
parikhitritgithub <parikhitkurmi44@gmail.com>
2024-03-05 13:48:14 +0530

    add fib

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,9 @@
-print("Hello World")
-
 def fibonacci(n):
     fib_seq = [0, 1]
     while len(fib_seq) < n:
         fib_seq.append(fib_seq[-1] + fib_seq[-2])
     return fib_seq
 
-
+# Example usage:
 n = int(input("Enter the number of Fibonacci terms: "))
 print(fibonacci(n))
